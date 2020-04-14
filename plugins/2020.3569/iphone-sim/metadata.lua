dfd52ff2e7bbe7dadb5c18ac4ef6d4c56e2a97f2
Vlad Shcherban <vlad@coronalabs.com>
2020-04-14 00:00:17 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDMintegralAdapter', },
+		frameworks = { 'MTGSDK', 'MTGSDKInterstitial', 'MTGSDKInterstitialVideo', 'MTGSDKReward', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
